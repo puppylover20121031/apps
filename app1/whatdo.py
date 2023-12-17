10decf1c26587710da23f1bcd273f3b92f3d2c28
puppylover20121031 <puppylove20121031@gmail.com>
2023-12-17 12:32:03 -0800

    added app1
    added app1 to start with

---
+++ app1/whatdo.py
@@ -0,0 +1,19 @@
+import random
+import json
+
+def whattodo():
+
+    with open('todolist.json', 'r') as f:
+        todo_list = json.load(f)
+
+        num_tasks = len(todo_list['unfinished'])
+
+        print(f"You have {num_tasks} tasks:")
+        for task_num, task_desc in todo_list['unfinished'].items():
+            print(f"{task_num}: {task_desc}")
+
+        if num_tasks >= 10:
+            print("That's a lot of tasks! Good luck!")
+
+
+whattodo()
