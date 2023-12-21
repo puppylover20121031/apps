0c8b603b0fa4b5a9e51a32bc774cfe630fc7ddc8
puppylover20121031 <puppylove20121031@gmail.com>
2023-12-21 01:01:35 -0800

    added tts

--- app1/main.py
+++ app1/main.py
@@ -1,10 +1,22 @@
 from whatdo import whattodo
+from TTS369 import TTS
 print("hello")
+TTS("hello").say()
 run = True
+attempts = 0
 while run:
-    cmd = input("what do want to do?\n")
+    if attempts >= 1:
+        TTS("what next").say()
+    else:
+        TTS("what do you want to do").say()
+    attempts += 1
+    cmd = input("what do you want to do?\n")
     match cmd:
         case "what do i do":
             whattodo()
+        case "play":
+            print("why do you want to play human i am a fucker.")
+            TTS("why do you want to play human i am a fucker.").say()
         case "":
             print("sorry you did not enter a command.")
+            TTS("sorry you did not enter a command.").say()
--- app1/whatdo.py
+++ app1/whatdo.py
@@ -1,19 +1,24 @@
 import random
 import json
+from TTS369 import TTS
 
 def whattodo():
 
-    with open('C:/Users/jaydenpuppy/Documents/GitHub/apps/app1/todolist.json', 'r') as f:
-        todo_list = json.load(f)
-
+    with open('C:/Users/jaydenpuppy/Documents/GitHub/apps/app1/todolist.json', 'r+') as f1:
+        todo_list = json.load(f1)
         num_tasks = len(todo_list['unfinished'])
-
         print(f"You have {num_tasks} tasks:")
+        TTS(f"you have {num_tasks} tasks").say()
         for task_num, task_desc in todo_list['unfinished'].items():
             print(f"{task_num}: {task_desc}")
-
+            TTS(f"{task_num}: {task_desc}").say()
         if num_tasks >= 10:
             print("That's a lot of tasks! Good luck!")
-
-
-whattodo()
+            TTS("That's a lot of tasks! Good luck!").say()
+        if todo_list["unfinished"].items() != enumerate(todo_list["unfinished"], 1):
+            print("oh no your number order is not correct i will fix it!")
+            TTS("oh no your number order is not correct, i will fix it").say()
+            fixed = sorted(todo_list["unfinished"].items())
+            print(str(fixed).replace("[(", "{").replace(")]", "}"))
+            f1.truncate(0)
+            f1.write(str(fixed).replace("[(", "{").replace(")]", "}").replace("(", "{").replace(")", "}"))
